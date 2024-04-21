fc99f7e0ea6202173268f04328ef03cbfe9654f3
PyNetExplorer <katyaportland@gmail.com>
2024-04-20 21:39:40 -0700

    Restaurant picker

--- restaurant_picker.py
+++ restaurant_picker.py
@@ -15,17 +15,37 @@ def choose_restaurant():
         "Forbidden City"
     ]
 
-    # Randomly choose a restaurant
-    chosen_restaurant = random.choice(restaurants)
+    count = 0
+    while count < 3:
+        # Randomly choose a restaurant
+        chosen_restaurant = random.choice(restaurants)
     
-    print("Welcome to the Tacoma Restaurant Chooser!")
-    answer=input("Are you hungry? yes/no\n>").lower()
-    if answer == "yes":
-        print("I suggest you try:", chosen_restaurant)
-    elif answer == "no":
-        print("Well, come back when you are hungry!")
-    else:
-        print("K, bye..")
+        print("Welcome to the Tacoma Restaurant Chooser!")
+        answer=input("Are you hungry? (yes/no)\n> ").lower()
 
+        if answer == "yes":
+            print("I suggest you try", chosen_restaurant)
+            choice=input("Do you like this choice? (yes/no)\n> ").lower()
+            if choice == "yes":
+                print("Enjoy!")
+                break
+            elif choice == "no":
+                count += 1
+                if count < 3:
+                    print("Really?? Well, than...")
+                    continue
+                else:
+                    print("Sorry, choose it yourself then.")
+                    break
+            else:
+                print("Type yes or no.")
+        elif answer == "no":
+            print("Well, come back when you are hungry!")
+            break
+        else:
+            print("K, bye..")
+            break
+
+    
 # Run the restaurant chooser app
 choose_restaurant()
